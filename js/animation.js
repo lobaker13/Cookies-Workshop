a1b8420d00e11e992a6b8d8e15b5e3b36c8b9c2e
Logan Baker <lobaker13@gmail.com>
2017-06-15 10:57:18 -0400

    some mediocre styling

--- js/animation.js
+++ js/animation.js
@@ -2,8 +2,9 @@ document.addEventListener("DOMContentLoaded", function(){
 document.querySelector("#checkout").addEventListener("click", function() {
   console.log("not hey");
   $("#checkout").animate({
-    marginLeft: '150%',
+    marginLeft: '200%',
   },3000,function(){
+    alert("Your Order Will be Ready Soon!");
     window.location.reload();
     });
   });
